edb8651016df21034dea7645ca748d21e4131248
Paul Tomchik <pauljtomchik@gmail.com>
2015-10-09 00:39:00 -0400

    Patches in watchers.js

--- src/watchers.js
+++ src/watchers.js
@@ -1,74 +1,112 @@
 'use strict';
 
+var http = require('http');
 
-var request = require('request');
 
 
-function MTA_Subway_SIRI_Server_watcher (sol_bot, log) {
-    var channel = sol_bot.getChannelByName('mta-gtfsr2siri') ,
-    //var channel = sol_bot.getChannelByName('sol-bot') ,  //For testing.
+function MTA_Subway_SIRI_Server_time_watcher (sol_bot, log) {
+    //var channel = sol_bot.getChannelByName('mta-gtfsr2siri') ,
+    var channel = sol_bot.getChannelByName('sol-bot') ,  /* jshint ignore:line */ //This channel is for testing.
 
         all_good = true ,
         retry    = 0    ,
 
-        last_timestamp = Number.NEGATIVE_INFINITY;
-
+        last_gtfsrt_timestamp = Number.NEGATIVE_INFINITY;
 
     setInterval(function () {
-        request('http://mars.availabs.org:16180/vehicle-monitoring', function (err, response, body) {
 
-            var this_timestamp;
+        http.get('http://mars.availabs.org:16180/admin/get/currentGTFSRealtimeTimestamp', function (res) {
+            res.on('data', function (d) {
+                var this_gtfsrt_timestamp;
 
-            console.log("Foo.");
-                
-            if (err) {
-                if (all_good) {
-                    log.error('ERROR: MTA_Subway_SIRI_Server is down.', { err: err });
-                } else {
-                    if ((++retry % 3) === 0) {
-                        log.error('ERROR: MTA_Subway_SIRI_Server is still down.', { err: err, retry: retry });
-                    }
-                }
-
-                if (retry === 10) {
-                    channel.send('The MTA_Subway_SIRI_Server is down.');
-                }
-
-                all_good = false;
-
-                return;
-            } 
-
-            if (response.statusCode === 200) {
                 try {
-                    this_timestamp = body.Siri.ServiceDelivery.ResponseTimestamp;
+                    this_gtfsrt_timestamp = parseInt(d.toString());
 
-                    if (this_timestamp <= last_timestamp) {
+                    if (this_gtfsrt_timestamp <= last_gtfsrt_timestamp) {
+
+                        last_gtfsrt_timestamp = this_gtfsrt_timestamp;
+
                         if ((retry++ % 4) === 0) {
                             all_good = false;
-                            log.warn({ msg: 'WARN: MTA_Subway_SIRI_Server not updating the realtime data.', retry: retry });
+                            log.warn({ 
+                                msg: 'WARN: MTA_Subway_SIRI_Server may not be updating the realtime data.', 
+                                retry: retry });
                         }
 
                         if (retry === 10) {
-                            channel.send('MTA_Subway_SIRI_Server is not updating the realtime data.');
+                            channel.send('MTA_Subway_SIRI_Server may not be updating the realtime data.');
                         }
 
                     } else {
                         all_good = true;
                         retry = 0;
-                        last_timestamp = this_timestamp;
+                        last_gtfsrt_timestamp = this_gtfsrt_timestamp;
                     }
                 } catch (e) {
+                    log.error(e);
+                }
+            });
+        }).on('error', function (e) {
+            log.error(e);
+            return; 
+        });
+    }, 30000);
+}
+
+
+function MTA_Subway_SIRI_Server_data_watcher (sol_bot, log) {
+    //var channel = sol_bot.getChannelByName('mta-gtfsr2siri') ,
+    var channel = sol_bot.getChannelByName('sol-bot') ,  // jshint ignore:line 
+
+        all_good = true ,
+        retry    = 0    ;
+
+
+    setInterval(function () {
+
+        http.get('http://mars.availabs.org:16180/vehicle-monitoring', function (res) {
+
+            var body = '';
+
+            res.on('data', function(d) {
+                body += d;
+            });
+
+            res.on('end', function() {
+                try {
+                    JSON.parse(body);
+                    all_good = true;
+                    retry = 0;
+                } catch (e) {
+                    log.error(e);
                     all_good = false;
                     if (retry++ === 10) {
                         channel.send('MTA_Subway_SIRI_Server is sending mangled data.');
                     } 
                 }
+            });
+
+        }).on('error', function (err) {
+
+            if (all_good) {
+                log.error('ERROR: MTA_Subway_SIRI_Server is down.', { err: err });
+            } else {
+                if ((++retry % 3) === 0) {
+                    log.error('ERROR: MTA_Subway_SIRI_Server is still down.', { err: err, retry: retry });
+                }
             }
+
+            if (retry === 10) {
+                channel.send('The MTA_Subway_SIRI_Server is down.');
+            }
+
+            all_good = false;
         });
+
     }, 30000);
 }
 
 module.exports = [
-    MTA_Subway_SIRI_Server_watcher, 
+    MTA_Subway_SIRI_Server_time_watcher, 
+    MTA_Subway_SIRI_Server_data_watcher, 
 ];
