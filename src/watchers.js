3bcd4536c156f0bdadb9a75f290a031a0e7aae4c
Paul Tomchik <pauljtomchik@gmail.com>
2015-10-29 14:11:07 -0400

    More thorough monitoring of MTA_Subway_SIRI_Server

--- src/watchers.js
+++ src/watchers.js
@@ -1,24 +1,75 @@
 'use strict';
 
-var request = require('request');
+// Threse are passed in via MTA_Subway_SIRI_Server_data_watcher
+var sol_bot,
+    log;
+
+var request = require('request'),
+    parseXML = require('xml2js').parseString;
+
+var stop_ids = require('./stopIDs');
 
 
 
+function MTA_Subway_SIRI_Server_data_watcher (_sol_bot, _log) {
+    sol_bot = _sol_bot;
+    log = _log;
 
-function MTA_Subway_SIRI_Server_data_watcher (sol_bot, log) {
+    var vehicleMonitoringURL_json = 'http://localhost:16180/api/siri/vehicle-monitoring.json',
+        vehicleMonitoringWithCallsURL_json = vehicleMonitoringURL_json + '?VehicleMonitoringDetailLevel=calls',
+
+        vehicleMonitoringURL_xml = 'http://localhost:16180/api/siri/vehicle-monitoring.xml',
+        vehicleMonitoringWithCallsURL_xml = vehicleMonitoringURL_xml + '?VehicleMonitoringDetailLevel=calls',
+
+        stopMonitoringURL_base =  'http://localhost:16182/api/siri/stop-monitoring',
+
+        i;
+
+    watcherFactory(vehicleMonitoringURL_json, 'json');
+    watcherFactory(vehicleMonitoringWithCallsURL_json, 'json');
+
+    watcherFactory(vehicleMonitoringURL_xml, 'xml');
+    watcherFactory(vehicleMonitoringWithCallsURL_xml, 'xml');
+
+
+    for ( i = 0; i < stop_ids.length; ++i ) {
+        watcherFactory(stopMonitoringURL_base + '.json?MonitoringRef=MTA%20' + stop_ids[i], 'json');   
+        watcherFactory(stopMonitoringURL_base + 
+                       '.json?StopMonitoringDetailLevel=calls&MonitoringRef=MTA%20' + 
+                       stop_ids[i], 'json');   
+
+        watcherFactory(stopMonitoringURL_base + '.xml?MonitoringRef=MTA%20' + stop_ids[i], 'xml');   
+        watcherFactory(stopMonitoringURL_base + 
+                       '.xml?StopMonitoringDetailLevel=calls&MonitoringRef=MTA%20' + 
+                       stop_ids[i], 'xml');   
+    }
+}
+
+
+function watcherFactory (url, format) {
     var channel = sol_bot.getChannelByName('sol-bot') ,  // jshint ignore:line 
 
         all_good      = true ,
         connect_retry = 0    ,
-        parsing_retry = 0    ,
+        parsing_retry = 0    ;
 	
-        vehicleMonitoringURL_json = 
-            'http://mars.availabs.org:16180/vehicle-monitoring.json';
 
+    function parsingErrorHandler (e, body) {
+        console.error('e:', e);
 
+        if ((connect_retry++ % 3) === 0) {
+            log.error('ERROR while parsing the response.', { body : body, retry: parsing_retry });
+        }
+        all_good = false;
+        if (parsing_retry++ === 10) {
+            channel.send('MTA_Subway_SIRI_Server is sending bad data.');
+        } 
+    }
+
+    console.log(url);
     setInterval(function () {
 
-        request(vehicleMonitoringURL_json, function (error, reponse, body) {
+        request(url, function (error, reponse, body) {
 
             if (error || ( reponse.statusCode !== 200 )) {
 
@@ -44,23 +95,24 @@ function MTA_Subway_SIRI_Server_data_watcher (sol_bot, log) {
             }
 
             try {
-                JSON.parse(body);
+                if (format === 'json') {
+                    console.log(JSON.parse(body));
+                } else {
+                    parseXML(body, function (e) {
+                        if (e) { 
+                            parsingErrorHandler(e, body); 
+                        }
+                    });
+                }
                 all_good = true;
                 parsing_retry = 0;
             } catch (e) {
-                console.error('e:', e);
-
-                if ((connect_retry++ % 3) === 0) {
-                    log.error('ERROR while parsing the response.', { body : body, retry: parsing_retry });
-                }
-                all_good = false;
-                if (parsing_retry++ === 10) {
-                    channel.send('MTA_Subway_SIRI_Server is sending bad data.');
-                } 
+                parsingErrorHandler(e, body);
             }
         });
         
     }, 30000);
 }
+
 
 module.exports = [ MTA_Subway_SIRI_Server_data_watcher, ];
