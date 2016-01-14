89aebbbf18d4dc411f3858c96c5374a4535c6218
paultomchik <paultomchik@gmail.com>
2016-01-13 23:32:07 -0500

    Better handling of toobusy errors on MTA_Subway_SIRI_Server.

--- src/watchers.js
+++ src/watchers.js
@@ -11,6 +11,13 @@ var stop_ids = require('./stopIDs');
 
 var sentMessage = false;
 
+var stopMonitoringCallsPerSecond = 20;
+
+var toobusyErrorMessage = "Server is temporarily too busy. Please try again.",
+    toobusyErrors = [],     // Holds posix timestamps of the toobusy errors.
+    TOOBUSY_THRESHOLD = 50; // Acceptable number of toobusy 503 errors per 100 seconds.
+
+
 function MTA_Subway_SIRI_Server_data_watcher (_sol_bot, _log) {
     sol_bot = _sol_bot;
     log = _log;
@@ -21,33 +28,31 @@ function MTA_Subway_SIRI_Server_data_watcher (_sol_bot, _log) {
         vehicleMonitoringURL_xml = 'http://localhost:16180/api/siri/vehicle-monitoring.xml',
         vehicleMonitoringWithCallsURL_xml = vehicleMonitoringURL_xml + '?VehicleMonitoringDetailLevel=calls';
 
-    watcherFactory(function () { return vehicleMonitoringURL_json; }, 'json', 30000);
-    watcherFactory(function () { return vehicleMonitoringWithCallsURL_json; }, 'json', 1000);
+    watcherFactory(function () { return vehicleMonitoringURL_json; }, 'json', 500);
+    watcherFactory(function () { return vehicleMonitoringWithCallsURL_json; }, 'json', 500);
 
-    watcherFactory(function () { return vehicleMonitoringURL_xml; }, 'xml', 30000);
-    watcherFactory(function () { return vehicleMonitoringWithCallsURL_xml; }, 'xml', 1000);
+    watcherFactory(function () { return vehicleMonitoringURL_xml; }, 'xml', 500);
+    watcherFactory(function () { return vehicleMonitoringWithCallsURL_xml; }, 'xml', 500);
 
-    watcherFactory(getRandomStopMonitoringURL.bind(null, 'json'), 'json', 50);   
-    watcherFactory(getRandomStopMonitoringURL.bind(null, 'xml'), 'xml', 50);   
+    watcherFactory(getRandomStopMonitoringURL.bind(null, 'json'), 'json', 1000 / stopMonitoringCallsPerSecond);   
+    watcherFactory(getRandomStopMonitoringURL.bind(null, 'xml'), 'xml', 1000 / stopMonitoringCallsPerSecond);   
 }
-
 
 
 function getRandomStopMonitoringURL (dataFormat) {
     return 'http://localhost:16180/api/siri/stop-monitoring.' + dataFormat + '?' + 
-            'MonitoringRef=MTA%20' + stop_ids[Math.floor(stop_ids.length * Math.random())] +
+            'MonitoringRef=MTA_' + stop_ids[Math.floor(stop_ids.length * Math.random())] +
             ((Math.random() > 0.5) ?  '&StopMonitoringDetailLevel=calls' : '');
 }
 
 
 
-function watcherFactory (urlGetter, format, timeout) {
+function watcherFactory (urlGetter, format, intervalTimeout) {
     var channel = sol_bot.getChannelByName('sol-bot') ,  // jshint ignore:line 
 
         all_good      = true ,
-        connect_retry = 0    ,
-        parsing_retry = 0    ,
-	counter       = 0    ;
+        connect_retry = 0 ,
+        parsing_retry = 0 ;
 	
 
     function parsingErrorHandler (e, body) {
@@ -70,67 +75,85 @@ function watcherFactory (urlGetter, format, timeout) {
 
         var url   = urlGetter();
 
-        request({
-                    url: url,
-                    //timeout: 10000,
-                }, 
+        request(
             
-                function (error, response, body) {
+            { url: url, }, 
+        
+            function (error, response, body) {
 
+                var timestamp = Math.floor(Date.now() / 1000),
+                    resBodyJSON;
 
-                    if (error || (!response) || (response.statusCode !== 200)) {
-
-			console.log(counter++, '======================================================');
-			console.log('url: ', url);
-			console.log("Error:", error, "; statusCode:", response && response.statusCode);
-
-                        if (response) {
-                            console.error('response.statusCode:', response.statusCode);
-                        }
-
-                        if (all_good) {
-                            log.error('ERROR: MTA_Subway_SIRI_Server is down.', { error: error });
-                        } else {
-                            if ((++connect_retry % 3) === 0) {
-                                log.error('ERROR: MTA_Subway_SIRI_Server is still down.', 
-                                            { error: error, retry: connect_retry });
-                            }
-                        }
-
-                        if (!sentMessage && (connect_retry === 10)) {
-                            sentMessage = true;
-                            //channel.send('The MTA_Subway_SIRI_Server is down.');
-                            console.log('The MTA_Subway_SIRI_Server is down.');
-                        }
-
-                        all_good = false;
-
-                        return;
-                    }
-
-                    connect_retry = 0;
-
-                    try {
-                        if (format === 'json') {
-                            JSON.parse(body);
-                        } else {
-                            parseXML(body, function (e) {
-                                if (e) { 
-                                    parsingErrorHandler(e, body); 
-                                }
-                            });
-                        }
-                        all_good = true;
-                        parsing_retry = 0;
-                    } catch (e) {
-                        parsingErrorHandler(e, body);
-                    } finally {
-                        body = null;
-                    }
+                toobusyErrors = toobusyErrors.filter(function (ts) {
+                    return (timestamp - ts) < 100;   // Filter out the timestamps older than 100 seconds.
                 });
+
+                if (error || (!response) || (response.statusCode !== 200)) {
+
+                    resBodyJSON = response && response.body && JSON.parse(response.body);
+
+                    if ( response && (response.statusCode === 503) && (resBodyJSON.error === toobusyErrorMessage)) {
+
+                        toobusyErrors.push(timestamp);
+
+                        if (toobusyErrors.length > TOOBUSY_THRESHOLD) {
+                            console.log("\n===== toobusy error rate exceeded threshold =====");
+                            console.log("\terror rate : " + toobusyErrors.length + " in last 100 sec)");
+                            console.log("\tthreshold  : " + TOOBUSY_THRESHOLD + "\n");
+                        }
+
+                    } else {
+                        console.log('===== server response error besides toobusy =====');
+                        console.log('url: ', url);
+                        console.log("Error:", error);
+                        console.log("response.statusCode", response && response.statusCode);
+                        console.log("response.body", response && response.body);
+                    }
+
+                    if (all_good) {
+                        log.error('ERROR: MTA_Subway_SIRI_Server is down.', { error: error });
+                    } else {
+                        if ((++connect_retry % 4) === 0) {
+                            log.error('ERROR: MTA_Subway_SIRI_Server is still down.', 
+                                        { error: error, retry: connect_retry });
+                        }
+                    }
+
+                    if (!sentMessage && (connect_retry === 10)) {
+                        sentMessage = true;
+                        //channel.send('The MTA_Subway_SIRI_Server is down.');
+                        console.log('The MTA_Subway_SIRI_Server is down.');
+                    }
+
+                    all_good = false;
+
+                    return;
+                }
+
+                connect_retry = 0;
+
+                try {
+                    if (format === 'json') {
+                        JSON.parse(body);
+                    } else {
+                        parseXML(body, function (e) {
+                            if (e) { 
+                                parsingErrorHandler(e, body); 
+                            }
+                        });
+                    }
+                    all_good = true;
+                    parsing_retry = 0;
+                } catch (e) {
+                    parsingErrorHandler(e, body);
+                } finally {
+                    body = null;
+                }
+            }
+        );
                 
-            }, timeout);
-    }
+    }, intervalTimeout);
+}
 
 
 module.exports = [ MTA_Subway_SIRI_Server_data_watcher, ];
